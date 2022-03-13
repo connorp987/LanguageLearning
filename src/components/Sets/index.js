0d0beb9eb0ee9a3b6b0ba4ca6ea362a592bfa4c4
Connor Plunkett <connor.plunkett@gmail.com>
2022-03-13 15:01:10 -0400

    basic flashcard in a set

--- src/components/Sets/index.js
+++ src/components/Sets/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { useLocation, Link } from "react-router-dom";
 import { FlashcardArray } from "react-quizlet-flashcard";
 import { Table, Input, Button } from 'antd';
 import { withAuthorization } from '../Session';
@@ -8,9 +7,51 @@ import axios from 'axios'
 class Sets extends Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+
     this.state = {
       cardData: {},
-      pageID: this.props.match.params.id
+      pageID: this.props.match.params.id,
+      tempStringOne: '',
+      tempStringTwo: '',
+      cards: [{
+        id: 1,
+        front: "What is the capital of <u>Alaska</u>?",
+        back: "Juneau",
+        frontChild: <div>Hello there</div>,
+        backChild: <p>This is a back child</p>
+      },
+      {
+        id: 2,
+        front: "What is the capital of California?",
+        back: "Sacramento",
+      },
+      {
+        id: 3,
+        front: "What is the capital of New York?",
+        back: "Albany",
+      },
+      {
+        id: 4,
+        front: "What is the capital of Florida?",
+        back: "Tallahassee",
+      },
+      {
+        id: 5,
+        front: "What is the capital of Texas?",
+        back: "Austin",
+      },
+      {
+        id: 6,
+        front: "What is the capital of New Mexico?",
+        back: "Santa Fe",
+      },
+      {
+        id: 7,
+        front: "What is the capital of Arizona?",
+        back: "Phoenix",
+      }]
     };
   }
 
@@ -29,10 +70,91 @@ class Sets extends Component {
       })
     //}
   }
+
+  handleChange(e) {
+    if (e.target.dataset.key === "firstBox") {
+      this.setState({tempStringOne: e.target.value})
+    }
+    if (e.target.dataset.key === "secondBox") {
+      this.setState({tempStringTwo: e.target.value})
+    }
+  }
+
+  handleKeyDown(e) {
+    if (e.key === 'Tab') {
+      if (e.target.dataset.key === "firstBox") {
+        this.setState({ tempStringOne: e.target.value })
+      }
+      if (e.target.dataset.key === "secondBox") {
+        this.setState({ tempStringTwo: e.target.value })
+      }
+      if (this.state.tempStringOne !== '' && this.state.tempStringTwo !== '') {
+
+        this.setState(cards => {
+          return {
+            cards: [
+              ...this.state.cards,
+              {
+                id: cards.length + 1,
+                front: this.state.tempStringOne,
+                back: this.state.tempStringTwo
+              }
+            ]
+          }
+        })
+
+      }
+
+      //console.log('do validate', this.state.cards);
+      //console.log(e.target.value);
+    }
+  }
   render() {
 
+    const columns = [
+      {
+        title: 'Front',
+        dataIndex: 'front',
+        key: 'front',
+        //render: text => <a>{text}</a>,
+      },
+      {
+        title: 'Back',
+        dataIndex: 'back',
+        key: 'back',
+      }
+    ];
     return (
-      <div>test</div>
+      <div>
+        <div style={{ marginLeft: '30%' }}>
+          <FlashcardArray cards={this.state.cards} />
+        </div>
+
+        
+        <Table pagination={false} columns={columns} dataSource={this.state.cards} />
+
+        <div style={{ marginLeft: "25%" }}>
+          <label>Enter a new term:</label>
+          <Input data-key="firstBox" style={{ width: "8%" }} type="text" onChange={this.handleChange} onKeyDown={this.handleKeyDown} />
+          <Input data-key="secondBox" style={{ width: "8%" }} type="text" onChange={this.handleChange} onKeyDown={this.handleKeyDown} />
+          <Button onClick={() => {
+            this.setState(cards => {
+              return {
+                cards: [
+                  ...this.state.cards,
+                  {
+                    id: cards.length + 1,
+                    front: this.state.tempStringOne,
+                    back: this.state.tempStringTwo
+                  }
+                ]
+              }
+            })
+          }
+          } >Add new word</Button>
+        </div>
+
+      </div>
     )
   }
 }
